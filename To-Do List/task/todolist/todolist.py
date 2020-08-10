c755e90ca63e10f9ea7605f6911a2858ed21b9b0
Osama Chelhaoui <osamachel02@gmail.com>
2020-08-10 13:58:34 +0200

    Update todolist.py

--- To-Do List/task/todolist/todolist.py
+++ To-Do List/task/todolist/todolist.py
@@ -26,12 +26,12 @@ session = Session()
 
 while True:
     print("""1) Today's tasks
-2) Week's tasks
-3) All tasks
-4) Missed tasks
-5) Add task
-6) Delete task
-0) Exit""")
+            2) Week's tasks
+            3) All tasks
+            4) Missed tasks
+            5) Add task
+            6) Delete task
+            0) Exit""")
 
     choice = int(input())
     if choice == 1:
